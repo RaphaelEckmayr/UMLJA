cf9781188ff73b5913b7960051f52464c7255eee
RaphaelEckmayr <Raphael.Eckmayr@gmail.com>
2019-11-18 12:14:37 +0100

    Added Final and Connections in Progress

--- venv/Include/Main.py
+++ venv/Include/Main.py
@@ -1,16 +1,20 @@
+
 import re
 import os
 import wx
 import ctypes.wintypes
 import ntpath
 
-#Classes
+
+# Classes
 class JaClass:
+    nodeId = ""
     className = ""
     variables = []
     methods = []
 
-    def __init__(self, className, variables, methods):
+    def __init__(self, nodeId, className, variables, methods):
+        self.nodeId = nodeId
         self.className = className
         self.variables = variables
         self.methods = methods
@@ -32,18 +36,25 @@ class Package:
         self.yCoordinate = float(yCoordinate)
         self.classes = []
 
+class Connection:
+    fromNode = ""
+    toNode = ""
+
+    def __init__(self, fromNode, toNode):
+        self.fromNode = fromNode
+        self.toNode = toNode
+
 
 class DnDPanel(wx.Panel):
     """"""
-    #Methods / Get attribute
+
+    # Methods / Get attribute
     def getAttributeFromGeometry(self, tag, string):
         result = re.findall("(?<=" + tag + "=\")(-*\d*.\d)", string)
         return result
 
     def printMessage(self, message):
         self.logTextCtrl.AppendText(message + '\n')
-
-
 
     # Parser
     def parse(self, filePath):
@@ -80,44 +91,62 @@ class DnDPanel(wx.Panel):
             else:
                 use = True
 
-        classNodes = re.findall("\<y\:UMLClassNode[^>]*>.*?\<\/y\:UMLClassNode\>", xmlFile)
 
-        for x in classNodes:
+        connections = []
+        connectionTags = re.findall("<edge[^>]*>", xmlFile)
+
+        for connection in connectionTags:
+            fromNode = re.findall('(?<=source=")[^"]*', connection)[0]
+            toNode = re.findall('(?<=target=")[^"]*', connection)[0]
+            connections.append(Connection(fromNode, toNode))
+
+        nodes = re.findall('<node id="n\d::n\d">.*?<\/node>',xmlFile) #Error in regex doess't select <node> Tag
+
+        for x in nodes:
             try:
-                nodeLabel = re.findall("<y\:NodeLabel[^>]*>[^<]+", x)[0]
-                className = re.findall("(?<=>)[^']*", nodeLabel)[0];
-                attributeLabelArray = re.findall("<y\:AttributeLabel[^>]*>[^<]+", x);
+                classId = re.findall('(?<=<node id=")[^"]+',x)[0]
+                if re.match("n\d::n\d", classId):
+                    classNode = re.findall("\<y\:UMLClassNode[^>]*>.*?\<\/y\:UMLClassNode\>", x)[0]
+                    nodeLabel = re.findall("<y\:NodeLabel[^>]*>[^<]+", classNode)[0]
+                    className = re.findall("(?<=>)[^']*", nodeLabel)[0];
+                    attributeLabelArray = re.findall("<y\:AttributeLabel[^>]*>[^<]+", x);
 
-                if (len(attributeLabelArray) == 1):
-                    attributeString = re.findall("(?<=>).*", attributeLabelArray[0])[0];
+                    if len(attributeLabelArray) == 1:
+                        attributeString = re.findall("(?<=>).*", attributeLabelArray[0])[0];
 
-                    attributeString = re.sub("\+", "\npublic", attributeString)
-                    attributeString = re.sub("-", "\nprivate", attributeString)
-                    attributeString = re.sub("#", "\nprotected", attributeString)
+                        attributeString = re.sub("\+", "\npublic ", attributeString)
+                        attributeString = re.sub("-", "\nprivate ", attributeString)
+                        attributeString = re.sub("#", "\nprotected ", attributeString)
 
-                    attributeString = re.sub("&lt;u&gt;", "static ", attributeString)
-                    attributeString = re.sub("&lt;[^&]*&gt;", "", attributeString)
+                        attributeString = re.sub("&lt;u&gt;", "static ", attributeString)
+                        attributeString = re.sub("&lt;[^&]*&gt;", "", attributeString)
 
-                    attributes = re.split("\n", attributeString)
+                        attributes = re.split("\n", attributeString)
 
-                    del attributes[0]
+                        del attributes[0]
 
-                    for i,a in enumerate(attributes):
-                        a = re.sub(":", "", a)
-                        temp = re.split(" ", a)
-                        for tempVar in temp:
-                            if tempVar == "":
-                                temp.remove(tempVar)
-                        try:
+                        for i, a in enumerate(attributes):
+                            a = re.sub(":", "", a)
+                            temp = re.split(" ", a)
+                            for tempVar in temp:
+                                if tempVar == "":
+                                    temp.remove(tempVar)
+
+                            isFinal = True
+                            for c in temp[1]:
+                                if c.islower():
+                                    isFinal = False
+                                    break
+                            if isFinal:
+                                temp[0] += " final"
+
                             if temp[1] == "static":
-                                a = temp[0]+ " " +temp[1] +" "+ temp[3] +" "+ temp[2]
+                                a = temp[0] + " " + temp[1] + " " + temp[3] + " " + temp[2]
                             else:
-                                a= temp[0] + " " + temp[3] + " " + temp[1]
-                        except:
-                            self.printMessage("Error with var length")
-                        attributes[i] = a
-                else:
-                    attributes = []
+                                a = temp[0] + " " + temp[2] + " " + temp[1]
+                            attributes[i] = a
+                    else:
+                        attributes = []
             except:
                 self.printMessage("Error with attribute definition: Layout probably wrong in" + x.className)
 
@@ -126,9 +155,9 @@ class DnDPanel(wx.Panel):
                 if len(methodLabelArray) == 1:
                     methodLabel = methodLabelArray[0]
                     methodString = re.findall("(?<=>).*", methodLabel)[0];
-                    methodString = re.sub("\+", "\npublic", methodString)
-                    methodString = re.sub("-", "\nprivate", methodString)
-                    methodString = re.sub("#", "\nprotected", methodString)
+                    methodString = re.sub("\+", "\npublic ", methodString)
+                    methodString = re.sub("-", "\nprivate ", methodString)
+                    methodString = re.sub("#", "\nprotected ", methodString)
 
                     methodString = re.sub("&lt;u&gt;", "static ", methodString)
                     methodString = re.sub("&lt;[^&]*&gt;", "", methodString)
@@ -143,30 +172,42 @@ class DnDPanel(wx.Panel):
                         for tempVar in temp:
                             if tempVar == "":
                                 temp.remove(tempVar)
-                        try:
-                            if temp[1] == "static":
-                                a = temp[0] + " " + temp[1] +" "+ temp[4] +" " + temp[2] + " " + temp[3]
+                        try: #Handle if Constructor
+                            if not temp[1][0].islower() and temp[1][1].islower():
+                                constructor = True
                             else:
-                                a = temp[0] + " " + temp[3] + " " + temp[1] + temp[2]
+                                constructor = False
+
+                            if temp[1] == "static":
+                                a = temp[0] + " " + temp[1] + " " + temp[len(temp)-1]+ " "
+                                for index in range(2,len(temp)-1):
+                                    a += temp[index] + " "
+                            elif constructor:
+                                a = ""
+                                for index in range(0,len(temp)):
+                                    a += temp[index] + " "
+                            else:
+                                a = temp[0] + " " + temp[len(temp)-1] + " " + temp[1] + " "
+                                for index in range(2,len(temp)-1):
+                                    a += temp[index] + " "
 
                             attributePart = re.findall("(?<=\()[^\)]*", a)[0]
-
                             attributesInMethod = re.split(",", attributePart)
-
                             fullattributes = ""
 
                             for cntr, attribute in enumerate(attributesInMethod):
-                                temp = re.split(" ", attribute)
-                                if cntr == 0:
-                                    fullattributes = temp[1] + " " + temp[0]
-                                else:
-                                    fullattributes += ", " + temp[1] + " " + temp[0]
+                                tempParts = re.split(" ", attribute)
+                                if len(tempParts) > 1:
+                                    if cntr == 0:
+                                        fullattributes = tempParts[1] + " " + tempParts[0]
+                                    else:
+                                        fullattributes += ", " + tempParts[2] + " " + tempParts[1] #Higher Index because of empty String
 
                             a = re.sub("(?<=\()[^\)]*", fullattributes, a)
-                        except:
-                            self.printMessage("Error with method length")
-                        methods[i] = a
 
+                            methods[i] = a
+                        except Exception as e:
+                            self.printMessage(str(e))
                 else:
                     methods = []
                     self.printMessage("No Methods in class")
@@ -187,7 +228,7 @@ class DnDPanel(wx.Panel):
 
                 if packageX <= xCoordinate and xCoordinate <= xMax and packageY <= yCoordinate and yCoordinate <= yMax:
                     newClasses = p.classes
-                    newClasses.append(JaClass(className, attributes, methods))
+                    newClasses.append(JaClass(classId, className, attributes, methods))
                     p.classes = newClasses
                     break
         # Compiler
@@ -204,7 +245,6 @@ class DnDPanel(wx.Panel):
 
             for c in p.classes:
                 directory = baselocation + filepath + c.className + ".java"
-                f = None
 
                 if not os.path.isfile(directory):
                     f = open(directory, "w")
@@ -214,6 +254,7 @@ class DnDPanel(wx.Panel):
                     print("File exists")
 
                 f.write("package " + p.packageName + ";\n")
+
                 f.write("public class " + c.className + "{\n")
 
                 for v in c.variables:
@@ -226,8 +267,7 @@ class DnDPanel(wx.Panel):
 
         self.printMessage("File successfully exported to " + baselocation)
 
-
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def __init__(self, parent):
         """Constructor"""
         wx.Panel.__init__(self, parent=parent)
@@ -235,10 +275,10 @@ class DnDPanel(wx.Panel):
         file_drop_target = MyFileDropTarget(self)
         self.lbl = wx.StaticText(self.panel, label="Drag some files here:")
         self.fileTextCtrl = wx.TextCtrl(self.panel,
-                                        style=wx.TE_MULTILINE|wx.HSCROLL|wx.TE_READONLY, size=(180, 70))
+                                        style=wx.TE_MULTILINE | wx.HSCROLL | wx.TE_READONLY, size=(180, 70))
         self.fileTextCtrl.SetDropTarget(file_drop_target)
         self.logLabel = wx.StaticText(self.panel, label="Log Console:")
-        self.logTextCtrl = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE|wx.HSCROLL|wx.TE_READONLY, size=(250, 150))
+        self.logTextCtrl = wx.TextCtrl(self.panel, style=wx.TE_MULTILINE | wx.HSCROLL | wx.TE_READONLY, size=(250, 150))
         self.button = wx.Button(self.panel, label="Convert")
         self.lblname = wx.StaticText(self.panel, label="Projektname:")
         self.editname = wx.TextCtrl(self.panel, size=(230, -1))
@@ -247,7 +287,7 @@ class DnDPanel(wx.Panel):
 
         self.fileTextCtrl.AppendText("Double Click to select File")
 
-        #Get MyDocuments Folder
+        # Get MyDocuments Folder
         CSIDL_PERSONAL = 5  # My Documents
         SHGFP_TYPE_CURRENT = 0  # Get current, not default value
         buf = ctypes.create_unicode_buffer(ctypes.wintypes.MAX_PATH)
@@ -283,8 +323,6 @@ class DnDPanel(wx.Panel):
         self.fileTextCtrl.Bind(wx.EVT_LEFT_DCLICK, self.openFileChooser)
         self.path = ''
 
-
-
     def convert(self, e):
         if len(self.editname.GetValue()) < 1:
             self.printMessage("Error: Field 'Project name' is empty")
@@ -296,7 +334,6 @@ class DnDPanel(wx.Panel):
         # otherwise ask the user what new file to open
         with wx.FileDialog(self, "Open .graphml file",
                            style=wx.FD_OPEN | wx.FD_FILE_MUST_EXIST) as fileDialog:
-
             if fileDialog.ShowModal() == wx.ID_CANCEL:
                 return  # the user changed their mind
 
@@ -307,29 +344,28 @@ class DnDPanel(wx.Panel):
         self.path = filePath
         self.fileTextCtrl.SetValue("Double Click to select File\n\nSelected File: " + ntpath.basename(filePath))
 
+
 class MyFileDropTarget(wx.FileDropTarget):
     """"""
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def __init__(self, window):
         """Constructor"""
         wx.FileDropTarget.__init__(self)
         self.window = window
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def OnDropFiles(self, x, y, filenames):
         """
         When files are dropped, write where they were dropped and then
         the file paths themselves
-
-
         self.window.SetInsertionPointEnd()
         self.window.updateText("\n%d file(s) dropped at %d,%d:\n" %
                               (len(filenames), x, y))
                               """
         print(filenames)
         print("\n%d file(s) dropped at %d,%d:\n" %
-                              (len(filenames), x, y))
+              (len(filenames), x, y))
         filepath = filenames[0]
         if not ntpath.basename(filepath).endswith(".graphml"):
             self.window.printMessage("Error: File extension must be: .graphml")
@@ -342,16 +378,17 @@ class MyFileDropTarget(wx.FileDropTarget):
 class DnDFrame(wx.Frame):
     """"""
 
-    #----------------------------------------------------------------------
+    # ----------------------------------------------------------------------
     def __init__(self):
         """Constructor"""
         wx.Frame.__init__(self, parent=None, title="UMLJA", size=wx.Size(440, 400))
         panel = DnDPanel(self)
         self.Show()
 
-#Running Code
 
-#----------------------------------------------------------------------
+# Running Code
+
+# ----------------------------------------------------------------------
 if __name__ == "__main__":
     app = wx.App(False)
     frame = DnDFrame()
